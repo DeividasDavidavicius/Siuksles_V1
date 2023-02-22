d611dd83d6c27efe714e011ad47e89db677b75bb
bumytv <lukas.skarulskis@ktu.edu>
2023-02-22 22:00:56 +0200

    V1.01 added unfinished menus to screen

--- app/src/main/java/com/example/siukslesv1/MainActivity.java
+++ app/src/main/java/com/example/siukslesv1/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.siukslesv1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,11 +11,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.siukslesv1.databinding.ActivityMainBinding;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     FirebaseAuth auth;
     Button button;
     TextView textView;
@@ -21,7 +26,21 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch(item.getItemId()) {
+                case R.id.home:
+                    break;
+                case R.id.camera:
+                    break;
+                case R.id.map:
+                    break;
+            }
+
+            return true;
+        });
 
         auth = FirebaseAuth.getInstance();
         button = findViewById(R.id.logout);
@@ -45,5 +64,11 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        //fragmentTransaction.replace(R.id.,fragment)
+
     }
 }
