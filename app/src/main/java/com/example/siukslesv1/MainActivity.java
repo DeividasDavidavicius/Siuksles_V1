5ea88eae7b3126992c0f2e85f023c195f177a7bb
dovink <dovydas.in@gmail.com>
2023-03-09 13:57:26 +0200

    Idetas setting menu, sutvarkytas navigation i ji, pakeista jo icona, perkelti mygtukai specialiai i settings, kad nesimatytu ju visuose laukuose

--- app/src/main/java/com/example/siukslesv1/MainActivity.java
+++ app/src/main/java/com/example/siukslesv1/MainActivity.java
@@ -5,12 +5,11 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import com.example.siukslesv1.databinding.ActivityMainBinding;
 import com.google.firebase.auth.FirebaseAuth;
@@ -50,9 +49,16 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        button1 = findViewById(R.id.settings);
 
-        button2 = findViewById(R.id.profile);
+        button1 = findViewById(R.id.Settings);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new SettingsFragment());
+            }
+        });
+        button2 = findViewById(R.id.Logout);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -61,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         auth = FirebaseAuth.getInstance();
-        button = findViewById(R.id.logout);
+        button = findViewById(R.id.Logout);
         user = auth.getCurrentUser();
 
         if(user == null) {
@@ -86,5 +92,19 @@ public class MainActivity extends AppCompatActivity {
         fragmentTransaction.replace(R.id.frameLayout,fragment);
         fragmentTransaction.commit();
 
+    }
+    public void logout(View view)
+    {
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(getApplicationContext(), Login.class);
+        startActivity(intent);
+        finish();
+
+    }
+    public void SwitchFragment(View view)
+    {
+        replaceFragment(new ProfileFragment());
     }
 }
--- app/src/main/java/com/example/siukslesv1/SettingsFragment.java
+++ app/src/main/java/com/example/siukslesv1/SettingsFragment.java
@@ -53,7 +53,8 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_settings, container,false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        return view;
     }
 }
