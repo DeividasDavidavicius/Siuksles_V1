acfb59fcec562d1d0cbf478a032dbfda350123dd
bumytv <lukas.skarulskis@ktu.edu>
2023-03-18 17:04:09 +0200

    Implemented the map into the application with zoom on Lithuania. You'll need to download a different emulator in order to run the app because Google Play Store is missing in the newest version. Here's the link: https://prnt.sc/H4oEDkTiDMiD

--- app/src/main/java/com/example/siukslesv1/mapActivity.java
+++ app/src/main/java/com/example/siukslesv1/mapActivity.java
@@ -9,17 +9,40 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.CancelableCallback;
+import com.google.android.gms.maps.GoogleMap.OnCameraIdleListener;
+import com.google.android.gms.maps.GoogleMap.OnCameraMoveCanceledListener;
+import com.google.android.gms.maps.GoogleMap.OnCameraMoveListener;
+import com.google.android.gms.maps.GoogleMap.OnCameraMoveStartedListener;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class mapActivity extends AppCompatActivity {
+
+public class mapActivity extends AppCompatActivity implements
+        OnMapReadyCallback,
+        OnCameraMoveStartedListener,
+        OnCameraMoveCanceledListener,
+        OnCameraIdleListener,
+        OnCameraMoveListener {
 
     Button settingsButton;
     Button profileButton;
+
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps);
+        mapFragment.getMapAsync(this);
 
         // Initialize and assign variable
         BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
@@ -67,5 +90,37 @@ public class mapActivity extends AppCompatActivity {
     private void switchToProfile() {
         Intent switchActivityIntent = new Intent(this, profileActivity.class);
         startActivity(switchActivityIntent);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        map = googleMap;
+
+        map.setOnCameraIdleListener(this);
+        map.setOnCameraMoveStartedListener(this);
+        map.setOnCameraMoveListener(this);
+        map.setOnCameraMoveCanceledListener(this);
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(54.8985, 23.9036), 6));
+    }
+
+    @Override
+    public void onCameraMoveStarted(int i) {
+
+    }
+
+    @Override
+    public void onCameraIdle() {
+
+    }
+
+    @Override
+    public void onCameraMoveCanceled() {
+
+    }
+
+    @Override
+    public void onCameraMove() {
+
     }
 }
