215f481641037114f25c45260c7b4a884817f17e
DeividasDavidavicius <101116531+DeividasDavidavicius@users.noreply.github.com>
2023-04-26 16:02:59 +0300

    Updated timer, added event creation and post deletion if they have more than 10 votes

--- app/src/main/java/com/example/siukslesv1/MainActivity.java
+++ app/src/main/java/com/example/siukslesv1/MainActivity.java
@@ -16,6 +16,7 @@ import android.os.Binder;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -44,6 +45,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
@@ -65,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
     DatabaseReference eventReference;
     List<Post> postList;
     TextView timer;
-    Date currentTime;
+    long votingTime;
     private String email;
     private Event event;
 
@@ -74,6 +76,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         auth = FirebaseAuth.getInstance();
         user = auth.getCurrentUser();
+        firebaseDatabase = FirebaseDatabase.getInstance("https://siuksliu-programele-default-rtdb.europe-west1.firebasedatabase.app/");
+        databaseReference = firebaseDatabase.getReference("posts");
+        eventReference = firebaseDatabase.getReference("events");
 
         if(user == null) {
             Intent intent = new Intent(getApplicationContext(), Login.class);
@@ -86,17 +91,34 @@ public class MainActivity extends AppCompatActivity {
         PostRecyclerView = findViewById(R.id.postRV);
 
         //TIMERIS (REIKES PAKEIST, DABAR LAIKINAS)
+
+        TimeZone tz = TimeZone.getTimeZone("Europe/Vilnius");
+        Calendar cal = Calendar.getInstance(tz);
+        cal.set(2023, 3, 30, 12, 0, 0);
+        long voteTime = cal.getTimeInMillis();
+
+        Calendar calendar = Calendar.getInstance(tz);
+        long currentTime = calendar.getTimeInMillis();
+
+
+        while(currentTime > voteTime)
+        {
+            voteTime += 604800000;
+        }
+
         timer = findViewById(R.id.timer);
-        long timerDuration = TimeUnit.MINUTES.toMillis(120);
+        long timerDuration = voteTime - currentTime;
         new CountDownTimer(timerDuration, 1000)
         {
 
             @Override
             public void onTick(long l) {
-                int hours = (int)(l/1000/60/60);
-                int minutes = (int) (l - hours * 60 * 60 * 1000)/(1000 * 60);
-                int seconds = (int) (l - minutes * 60 * 1000 - hours * 60 * 60 * 1000) / 1000;
-                timer.setText(hours + "h "+ minutes + "min " + seconds + "s");
+                int days = (int)(l/1000/60/60/24);
+                int l2 = (int) (l - days * 1000 * 60 * 60 * 24);
+                int hours = (int)(l2/1000/60/60);
+                int minutes = (int) (l2 - hours * 60 * 60 * 1000)/(1000 * 60);
+                int seconds = (int) (l2 - minutes * 60 * 1000 - hours * 60 * 60 * 1000) / 1000;
+                timer.setText(days+ "d " + hours + "h "+ minutes + "min " + seconds + "s");
             }
 
             @Override
@@ -105,17 +127,69 @@ public class MainActivity extends AppCompatActivity {
             }
         }.start();
 
+
+        timerReference = firebaseDatabase.getReference("timer");
+        //timerReference.child("timerDate").setValue(cal.getTimeInMillis());
+        timerReference.child("timerDate").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                votingTime = dataSnapshot.getValue(Long.class);
+                long currentTimeMillis = Calendar.getInstance().getTimeInMillis();
+
+                if(currentTimeMillis > votingTime)
+                {
+                    timerReference.child("timerDate").setValue(votingTime + 604800000);
+
+                    databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@android.support.annotation.NonNull DataSnapshot dataSnapshot) {
+                            for(DataSnapshot postSnapShot : dataSnapshot.getChildren()) {
+                                String postId = postSnapShot.getKey();
+                                DatabaseReference postRef = firebaseDatabase.getReference().child("posts").child(postId);
+                                postRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@android.support.annotation.NonNull DataSnapshot dataSnapshot) {
+                                        Post post = dataSnapshot.getValue(Post.class);
+                                        if (post != null) {
+                                            int postVoteCount = post.getVoteCount();
+                                            if (postVoteCount >= 10)
+                                            {
+                                                String eventID = UUID.randomUUID().toString();
+                                                Calendar calendar = Calendar.getInstance();
+                                                long currentEventTime = calendar.getTimeInMillis();
+                                                event = new Event(post.getEmail(), post.getName(), post.getLocation(), post.getUri(), post.getType(), currentEventTime, eventID);
+                                                String keyID = eventReference.push().getKey();
+                                                eventReference.child(keyID).setValue(event);
+                                                postRef.removeValue(); // Delete the post from Firebase
+                                            }
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@android.support.annotation.NonNull DatabaseError databaseError) {
+                                    }
+                                });
+                            }
+                        }
+                        @Override
+                        public void onCancelled(@androidx.annotation.NonNull DatabaseError error) {
+
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // This method is called when the read operation is cancelled.
+                System.out.println("Read cancelled: " + databaseError.getMessage());
+            }
+        });
+
         PostRecyclerView.setHasFixedSize(true);
         PostRecyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
-        firebaseDatabase = FirebaseDatabase.getInstance("https://siuksliu-programele-default-rtdb.europe-west1.firebasedatabase.app/");
-        databaseReference = firebaseDatabase.getReference("posts");
-        eventReference = firebaseDatabase.getReference("events");
-
-        // TIMER LEFT
-        currentTime = Calendar.getInstance().getTime();
-        timerReference = firebaseDatabase.getReference("timer");
-        timerReference.child("timerDate").setValue(currentTime.toString());
-
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -196,7 +270,6 @@ public class MainActivity extends AppCompatActivity {
             finish();
         }
 
-
         // NUOTRAUKOS
 
         //selectedImage = findViewById(R.id.displayImageView);
@@ -209,13 +282,15 @@ public class MainActivity extends AppCompatActivity {
             }
         }); */
 
+        /*
         email = user.getEmail();
         String eventID = UUID.randomUUID().toString();
         Calendar calendar = Calendar.getInstance();
         long currentEventTime = calendar.getTimeInMillis();
+        //Log.d("tag", String.valueOf("Laikas" + currentEventTime));
         event = new Event(email, "Test1223", "Location", "https://firebasestorage.googleapis.com/v0/b/siuksliu-programele.appspot.com/o/images%2FJPEG_20230319_192028.jpg?alt=media&token=ec74a9ca-5983-453e-8c9f-93b0c7f6f656", 1, currentEventTime, eventID);
         String keyID = eventReference.push().getKey();
-        eventReference.child(keyID).setValue(event);
+        eventReference.child(keyID).setValue(event); */
     }
     private void switchToSettings() {
         Intent switchActivityIntent = new Intent(this, settingsActivity.class);
